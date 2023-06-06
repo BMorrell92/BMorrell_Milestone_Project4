149df84d29967a24e881f0fc7fc03ed4c7e682b9
BMorrell92 <ben_morrell92@outlook.com>
2023-06-06 20:00:39 +0000

    added test cases to the products and checkouts app

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,59 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.messages import get_messages
+from decimal import Decimal
 
-# Create your tests here.
+from .models import Product, Order
+
+
+class CheckoutTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+        # Create a test product
+        self.product = Product.objects.create(
+            name='Test Product',
+            price=Decimal('9.99'),
+            description='This is a test product.'
+        )
+
+    def test_checkout_process(self):
+        # Log in the test user
+        self.client.login(username='testuser', password='testpassword')
+
+        # Add the product to the user's shopping cart
+        self.client.post(reverse('add_to_cart', args=[self.product.id]))
+
+        # Retrieve the shopping cart
+        response = self.client.get(reverse('view_cart'))
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the shopping cart contains the correct product
+        self.assertContains(response, self.product.name)
+
+        # Proceed to the checkout
+        response = self.client.get(reverse('checkout'))
+        self.assertEqual(response.status_code, 200)
+
+        # Submit the order
+        response = self.client.post(reverse('checkout'), {'payment_method': 'stripe'})
+        self.assertEqual(response.status_code, 302)
+
+        # Check that the order has been created
+        orders = Order.objects.filter(user=self.user)
+        self.assertEqual(orders.count(), 1)
+        order = orders.first()
+
+        # Check that the order total is correct
+        self.assertEqual(order.total, self.product.price)
+
+        # Check that a success message is displayed
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Order placed successfully.')
+
+        # Check that the shopping cart is now empty
+        response = self.client.get(reverse('view_cart'))
+        self.assertNotContains(response, self.product.name)
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Product
+
+class ProductTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_product_list_view(self):
+        # Create some test products
+        product1 = Product.objects.create(name='Product 1', price=10.99)
+        product2 = Product.objects.create(name='Product 2', price=15.99)
+
+        # Access the product list view
+        response = self.client.get(reverse('product_list'))
+        self.assertEqual(response.status_code, 200)
+
+        # Check that both products are listed
+        self.assertContains(response, product1.name)
+        self.assertContains(response, product2.name)
+
+    def test_product_detail_view(self):
+        # Create a test product
+        product = Product.objects.create(name='Test Product', price=9.99)
+
+        # Access the product detail view
+        response = self.client.get(reverse('product_detail', args=[product.id]))
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the product details are displayed correctly
+        self.assertContains(response, product.name)
+        self.assertContains(response, product.price)
+
+    def test_add_to_cart_view(self):
+        # Create a test product
+        product = Product.objects.create(name='Test Product', price=9.99)
+
+        # Log in the test user
+        self.client.login(username='testuser', password='testpassword')
+
+        # Add the product to the user's shopping cart
+        response = self.client.post(reverse('add_to_cart', args=[product.id]))
+        self.assertEqual(response.status_code, 302)
+
+        # Check that the product is added to the shopping cart
+        self.assertEqual(len(self.client.session['cart']), 1)
+        self.assertEqual(self.client.session['cart'][0]['product_id'], product.id)
+
+    # Add more test cases as needed for other features of the products section
+
